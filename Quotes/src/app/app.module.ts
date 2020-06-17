c00e4803499833930a23aa67d984e9483ea7ba5a
trever <toakumu@gmail.com>
2020-06-17 14:39:28 +0300

    added form component

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { HighlightDirective } from './highlight.directive';
+import { QoutesFormComponent } from './qoutes-form/qoutes-form.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    HighlightDirective,
+    QoutesFormComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
