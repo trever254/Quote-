47417e09722964036d6d9e6754bfc9c525cc517e
trever <toakumu@gmail.com>
2020-06-17 10:50:34 +0300

    added buttons

---
+++ Quotes/src/app/quotes.ts
@@ -0,0 +1,13 @@
+export class Quotes{
+    public upvotes :number
+    public downvotes :number
+    public myDate:Date
+    constructor(public name:string, public author:string, public quote:string){
+        this.upvotes=0
+        this.downvotes=0
+        this.myDate= new Date()
+
+    }
+}
+
+
---
+++ Quotes/src/app/quotes/quotes.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote';
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
+export class QuotesComponent implements OnInit {
+  quotes=[new Quote("No pain no gain","Do not judge a book by its cover","Never give up"),
+  new Quote("Always make your dreams become alive","Your dreams are valid","don't be obsessed with looking successful")]
+    preNum:number
+    lastNum:number
+    counter:number
+
+    addQuote(emittedQuote){
+      this.quotes.push(emittedQuote)
+    }
+  
+    upvote(i){
+        this.quotes[i].upvotes+=1
+    }
+    downvote(i){
+      this.quotes[i].downvotes+=1
+    }
+    delQuote(i){
+      this.quotes.splice(i, 1)
+    }
+    highestUpvote(){
+      this.preNum = 0
+      this.lastNum = 0
+  
+      for(this.counter=0 ; this.counter < this.quotes.length; this.counter++) {
+        this.lastNum = this.quotes[this.counter].upvotes;
+        if(this.lastNum > this.preNum){this.preNum = this.lastNum}
+      }
+      return  this.preNum
+    }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
